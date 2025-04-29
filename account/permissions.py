1ae34d27e68872b54c598bbdfba508799ee7fc6d
Tech-Forge18 <brightmindcollectives1@gmail.com>
2025-04-29 12:46:30 +0545

    commit

--- account/permissions.py
+++ account/permissions.py
@@ -3,12 +3,18 @@ from rest_framework import permissions
 
 class IsCustomer(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'customer'
+        return True  # Allow all permissions
+       # return request.user.is_authenticated and request.user.role == 'customer'
 
 class IsApprovedVendor(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'vendor'
+
+        return True  # Allow all permissions
+        #return request.user.is_authenticated and request.user.role == 'vendor'
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+
+        
+        return True  # Allow all permissions
+        #return request.user.is_authenticated and request.user.role == 'admin'
